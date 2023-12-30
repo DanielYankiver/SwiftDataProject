4b6036d577731cd12cef8b7b2b2815e548b09cd2
Daniel Yankiver <dyankiver@gmail.com>
2023-12-30 13:50:23 -0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Daniel Yankiver on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
